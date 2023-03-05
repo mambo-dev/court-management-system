ca4ebfefc8849a5759bb1bc1e73a90d263e17976
mambo-dev <mambo.michael.22@gmail.com>
2023-03-05 15:29:58 +0300

    start hearings module

--- src/pages/dashboard/hearings.tsx
+++ src/pages/dashboard/hearings.tsx
@@ -4,7 +4,11 @@ import DashboardLayout from "../../../components/layout/dashboard";
 type Props = {};
 
 export default function Hearings({}: Props) {
-  return <div>Hearings</div>;
+  return (
+    <div className="w-full h-full flex flex-col gap-y-4 px-2 py-4">
+      <div className="w-full flex items-center justify-end "></div>
+    </div>
+  );
 }
 
 Hearings.getLayout = function getLayout(page: React.ReactElement) {
